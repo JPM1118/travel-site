7e9821cdec2c2c6e90056bd4fcfef35a35628705
Jack <johnmccormick1118@gmail.com>
2018-07-02 15:43:23 -0500

    PostCSS, BEM,and other random stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,10 @@
 let gulp = require('gulp'),
-watch = require('gulp-watch');
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+nested = require('postcss-nested'),
+cssImport = require('postcss-import');
 
 gulp.task('default', ()=>{
     console.log("woop woop");
@@ -9,7 +14,10 @@ gulp.task('html', () => {
     console.log('blah blah');
 })
 gulp.task('styles', () => {
-    return gulp.src('./app/assets/styles/styles.css').pipe(gulp.dest('./app/temp/styles'));
+    return gulp.src('./app/assets/styles/styles.css')
+        
+        .pipe(postcss([cssImport, cssvars, autoprefixer, nested]))
+        .pipe(gulp.dest('./app/temp/styles'));
 });
 gulp.task('watch', () => {
     watch('./app/index.html', ()=>{
