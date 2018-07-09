e27bb438e525224fc0b4c833c63eda4c4bc5f740
Jack <johnmccormick1118@gmail.com>
2018-07-09 18:18:24 -0500

    Many changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,6 @@
-let gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer = require('autoprefixer'),
-cssvars = require('postcss-simple-vars'),
-nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-gulp.task('default', ()=>{
-    console.log("woop woop");
-})
 
-gulp.task('html', () => {
-    console.log('blah blah');
-})
-gulp.task('styles', () => {
-    return gulp.src('./app/assets/styles/styles.css')
-        
-        .pipe(postcss([cssImport, cssvars, autoprefixer, nested]))
-        .pipe(gulp.dest('./app/temp/styles'));
-});
-gulp.task('watch', () => {
-    watch('./app/index.html', ()=>{
-        gulp.start('html');
-    })
-    watch('./app/assets/styles/**/*.css', ()=>{
-        gulp.start('styles');
-    })
-})
+
+
